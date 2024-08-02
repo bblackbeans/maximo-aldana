60cff00a0246844a0f3a5fdcf8e4ff69dcf1e77e
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-02 14:02:21 -0300

    view: adicionando secoes na página sobre

--- sobre-inc.php
+++ sobre-inc.php
@@ -2,13 +2,13 @@
 /*
 Template Name: Sobre Incorporadora
 */
-include('header-inc.php'); ?>
+include('header-sobre-inc.php'); ?>
 
 <section id="banner" class="py-5">
     <div class="container">
         <div class="container-banner">
-            <div class="container-banner__title" >
-                <h1 style="text-align: start;">Somos <br> &ensp;&ensp; Maximo </h1>
+            <div class="container-banner-sobre__title">
+                <h1>Somos <br> &ensp;&ensp; Maximo </h1>
             </div>
 
         </div>
@@ -17,6 +17,107 @@ include('header-inc.php'); ?>
 
 </main>
 
+<section class="container" id="somos-maximo">
+    <div class="container-btns-somos-maximo">
+        <button class="btn-somos-maximo">PORTFÓLIO</button>
+        <button class="btn-somos-maximo">NOSSOS CLIENTES</button>
+        <button class="btn-somos-maximo">CERTIFICAÇÕES</button>
+        <button class="btn-somos-maximo">ORCE SUA OBRA</button>
+    </div>
+    <div class="container-content-somos-maximo">
+        <h1>#Somos<span>Maximo</span></h1>
+        <p>Com mais de 25 anos de mercado a Maximo Aldana traduz diariamente seu DNA em
+            cada projeto que realiza e se encontra hoje no seleto grupo das 100 maiores
+            Construtoras do Brasil segundo Ranking INTEC (Informações Técnicas da Construção).</p>
+        <p>Atuando no segmento de engenharia e construção civil desde a fundação,
+            nos destacamos pela cultura proativa, sempre com sangue no olho para direcionar todos
+            os nossos esforços para encantar nossos clientes. Com nossa Gestão Inteligente e nosso
+            Senso de Urgência, nos tornamos referência nos segmentos de obras prediais, hospitalares,
+            corporativas e retrofits.</p>
+        <p>O Setor Imobiliáro</p>
+        <p>Em 2012, iniciamos a unidade de negócio focada no desenvolvimento imobiliário das regiões do
+            Grande ABC, São Paulo e Litoral, construindo e incorporando nossos próprios empreendimentos
+            com foco em entregar excelência em cada detalhe de nossos produtos.</p>
+    </div>
+</section>
+
+<section id="proposito">
+    <div class="container container-content-text">
+        <h1>Propósito, Visão e Valores.</h1>
+        <div class="container-content-proposito">
+            <h4>Nosso</h4>
+            <h1>Propósito</h1>
+            <p>Transformar o Brasil através da construção civil com processos sustentáveis e inovadores.</p>
+        </div>
+        <div class="container-content-visao">
+            <h4>Nossa</h4>
+            <h1>Visão</h1>
+            <p>Ser referência no mercado da construção residencial, comercial e hospitalar em todo o território nacional.</p>
+            <p>Construir oportunidades de investimentos seguros em imóveis, garantindo retorno aos investidores e melhorando a qualidade de vida dos clientes.</p>
+        </div>
+    </div>
+</section>
+
+<section id="nossos-valores">
+    <div class="container">
+        <div class="titles-nossos-valores">
+            <h4>Nossos</h4>
+            <h1>Valores</h1>
+        </div>
+
+        <h1 class="title-section-nosso-valores">Aqui praticamos o <span>Max Mentality</span></h1>
+
+        <div>
+            <div>
+                <div>
+                    <h2>Cliente no Centro</h2>
+                    <p>Trabalhamos intensamente para priorizar a satisfação dos nossos clientes em cada entrega, 
+                        cumprindo sempre o que prometemos dentro dos prazos acordados, com a máxima qualidade.</p>
+                </div>
+                <div>
+                    <h2>DNA da construção</h2>
+                    <p>Desde sua fundação, a “obra” corre em nosso sangue. Ter o “pé na obra” é fundamental para 
+                        entregar o melhor resultado.</p>
+                </div>
+                <div>
+                    <h2>Fazer por merecer</h2>
+                    <p>Ter atitudes que merecem o mérito, fazendo sempre algo bom além dos parâmetros aceitos.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h2>Segurança não se negocia</h2>
+                    <p>Cultivamos o envolvimento e comprometimento das lideranças e profissionais operacionais 
+                        para garantirem o máximo de segurança no trabalho em nossos canteiros.</p>
+                </div>
+                <div>
+                    <h2>#OrgulhoMaximoAldana</h2>
+                    <p>Prezamos e valorizamos o lado humano da nossa equipe, buscando seu constante desenvolvimento 
+                        e criando um ambiente equilibrado e empático onde se sintam orgulhosos de fazer parte.</p>
+                </div>
+                <div>
+                    <h2>Ética e transparência</h2>
+                    <p>Valorizamos o respeito e a honestidade em nossas relações com clientes, fornecedores, 
+                        colaboradores e investidores, com o objetivo de formar alianças duradouras.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h2>Desenvolvimento Social e Sustentável</h2>
+                    <p>Somos engajados com a responsabilidade ambiental e contribuímos para a conservação 
+                        da natureza em todas as obras, investindo em tecnologias que colaboram para a construção 
+                        de projetos cada vez mais responsáveis</p>
+                </div>
+                <div>
+                    <h2>Gerar valor e inovação através da Engenharia</h2>
+                    <p>Inovação e criatividade são comportamentos intrínsecos, onde todos podem exercer a engenharia 
+                        para trazer valor aos nossos clientes.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
 
 <section id="blog-home" class="pb-5">
     <div class="container-fluid px-2">
@@ -40,7 +141,7 @@ include('header-inc.php'); ?>
                         <p>3 de Março de 2023 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
@@ -62,7 +163,7 @@ include('header-inc.php'); ?>
                         <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
@@ -84,7 +185,7 @@ include('header-inc.php'); ?>
                         <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
@@ -108,7 +209,7 @@ include('header-inc.php'); ?>
                         <p>3 de Março de 2023 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
@@ -130,7 +231,7 @@ include('header-inc.php'); ?>
                         <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
@@ -152,7 +253,7 @@ include('header-inc.php'); ?>
                         <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
                     </div>
                     <div class="card-blog__desc">
-                    Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
                     </div>
                     <div class="card-blog__button">
                         <a href="" class="bg-acquagreen">LEIA MAIS</a>
