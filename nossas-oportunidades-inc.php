e3a5060289682d859f7cbaa16cc4a671959d12e8
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-05 17:17:48 -0300

    view: criacao das páginas de governanca e nossas oportunidades

---
+++ nossas-oportunidades-inc.php
@@ -0,0 +1,258 @@
+<?php
+/*
+Template Name: Nossas Oportunidades - Inc
+*/
+include('header-oportunidades-inc.php'); ?>
+
+<section id="banner" class="py-5">
+    <div class="container-fluid">
+        <div class="container-banner">
+            <div class="container-banner-oportunidades__title">
+                <h1>Conheça nossas <br> &ensp;&ensp; Oportunidades </h1>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+</main>
+
+<section class="container" id="somos-maximo">
+    <div class="container-btns-somos-maximo">
+        <button class="btn-somos-maximo">PORTFÓLIO</button>
+        <button class="btn-somos-maximo">NOSSOS CLIENTES</button>
+        <button class="btn-somos-maximo">CERTIFICAÇÕES</button>
+        <button class="btn-somos-maximo">ORCE SUA OBRA</button>
+    </div>
+    <div class="container-content-oportunidades">
+        <h1>Nossas oportunidades</h1>
+        <h2>Venha fazer parte do nosso time! Confira as oportunidades e candidata-se</h2>
+    </div>
+</section>
+
+<section id="vagas">
+    <div class="container container-filtrar-opotunidades">
+        <p>Encontre sua vaga</p>
+        <button class="bt-oportunidades-filtrar" id="filterBtn">FILTRAR &ensp; <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ai ai-SettingsHorizontal">
+                <path d="M3 5h4m14 0H11m-8 7h12m6 0h-2M3 19h2m16 0H9" />
+                <circle cx="9" cy="5" r="2" />
+                <circle cx="17" cy="12" r="2" />
+                <circle cx="7" cy="19" r="2" />
+            </svg></button>
+        <h1>Conheça nossas vagas</h1>
+        <div class="accordion" id="accordion-vagas-maximo" style="width: 100%;">
+            <div class="accordion-item" style="margin-bottom: 2rem;">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                        Título vaga 1
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item" style="margin-bottom: 2rem;">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <span class="material-symbols-outlined">
+                            arrow_right
+                        </span> Título vaga 2
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item" style="margin-bottom: 2rem;">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Título vaga 3
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-nao-a-vagas">
+            <h2>Não há vagas no momento!</h2>
+            <p class="paragrafo-nao-a-vagas">Responda o formulário "Junte-se ao nosso time", quando tivermos vagas disponíveis do seu perfil, entraremos em contato</p>
+            <button>JUNTE-SE AO NOSSO TIME <i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+    </div>
+</section>
+
+
+<section id="oportunidades-somos-maximos">
+    <div class="container-fluid container-historia-maximos">
+        <h1>#SomosMaximo</h1>
+        <p>Com mais de 25 anos de mercado a Maximo Aldana traduz diariamente seu DNA em
+            cada projeto que realiza e se encontra hoje no seleto grupo das 100 maiores
+            Construtoras do Brasil segundo Ranking INTEC (Informações Técnicas da Construção).</p>
+        <p>Atuando no segmento de engenharia e construção civil desde a fundação,
+            nos destacamos pela cultura proativa, sempre com sangue no olho para direcionar todos
+            os nossos esforços para encantar nossos clientes. Com nossa Gestão Inteligente e nosso
+            Senso de Urgência, nos tornamos referência nos segmentos de obras prediais, hospitalares,
+            corporativas e retrofits.</p>
+        <p>O Setor Imobiliáro</p>
+        <p>Em 2012, iniciamos a unidade de negócio focada no desenvolvimento imobiliário das regiões do
+            Grande ABC, São Paulo e Litoral, construindo e incorporando nossos próprios empreendimentos
+            com foco em entregar excelência em cada detalhe de nossos produtos.</p>
+    </div>
+</section>
+
+<section id="blog-home" class="pb-5">
+    <div class="container-fluid px-2">
+        <div class="container-title-blog">
+            <h4>Fique por dento do</h4>
+            <h2>Nosso Blog</h2>
+        </div>
+        <div class="container container-card-blog">
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/novoContrato.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana pela terceira vez consecutiva, entre as 100 maiores construtoras do Brasil segundo o Ranking Intec</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>3 de Março de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/100maiores.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana construirá o novo Hospital de Unimed Sul Mineira em Pouso Alegre - MG</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/premiohealtharq.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana é destaque no HealthARQ 2022</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container container-card-blog second-card-blog" style="display: none;">
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/novoContrato.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana pela terceira vez consecutiva, entre as 100 maiores construtoras do Brasil segundo o Ranking Intec</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>3 de Março de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/100maiores.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana construirá o novo Hospital de Unimed Sul Mineira em Pouso Alegre - MG</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/premiohealtharq.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana é destaque no HealthARQ 2022</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-button-todos">
+            <button class="ver-todos-button bg-acquagreen" onclick="toggleCardBlog()">VER TODOS</button>
+        </div>
+    </div>
+</section>
+
+<?php include('footer-inc.php'); ?>
