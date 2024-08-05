e30b26dcbf9529d31b2e4b97482bcc4861b3f1f8
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-05 17:18:37 -0300

    criacao da página de portal, scripts de acordion e nao a vagas e imagens

--- src/script.js
+++ src/script.js
@@ -1,140 +1,165 @@
 // Definindo a função toggleDisplay para abrir todos os cards
 function toggleDisplay() {
-    var carousels = document.querySelectorAll('.secondCarousel');
-    
-    carousels.forEach(carousel => {
-        if (carousel.style.display === 'none' || carousel.style.display === '') {
-            carousel.style.display = 'block';
-        } else {
-            carousel.style.display = 'none';
-        }
-    });
+  var carousels = document.querySelectorAll(".secondCarousel");
+
+  carousels.forEach((carousel) => {
+    if (carousel.style.display === "none" || carousel.style.display === "") {
+      carousel.style.display = "block";
+    } else {
+      carousel.style.display = "none";
+    }
+  });
 }
 
 function toggleCardBlog() {
-    var carousels = document.querySelectorAll('.second-card-blog');
-    
-    carousels.forEach(carousel => {
-        if (carousel.style.display === 'none' || carousel.style.display === '') {
-            carousel.style.display = 'flex';
-        } else {
-            carousel.style.display = 'none';
-        }
-    });
+  var carousels = document.querySelectorAll(".second-card-blog");
+
+  carousels.forEach((carousel) => {
+    if (carousel.style.display === "none" || carousel.style.display === "") {
+      carousel.style.display = "flex";
+    } else {
+      carousel.style.display = "none";
+    }
+  });
 }
 
+// Funcao para acordeon de vagas
+function controlarVisibilidade() {
+  const accordion = document.getElementById("accordion-vagas-maximo");
+  const divNaoVagas = document.querySelector(".container-nao-a-vagas");
+  const itemsAccordion = accordion.querySelectorAll(".accordion-item");
+
+  if (itemsAccordion.length > 0) {
+    divNaoVagas.style.display = "none";
+  } else {
+    accordion.style.display = "none";
+    divNaoVagas.style.display = "flex";
+  }
+}
+
+// Chame a função após o carregamento da página
+window.onload = controlarVisibilidade;
 
 /* ESTILIZAÇÃO DO HEADER QUANDO A JANELA ROLA */
 
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('header');
-    const logo = document.getElementById('logoPrincipal');
-    if (window.scrollY > 0) {
-        header.classList.add('scrolled');
-        logo.src = './src/img/logo-color.png'; 
-    } else {
-        header.classList.remove('scrolled');
-        logo.src = './src/img/incorporadora-horizontal.png'; 
-    }
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  const logo = document.getElementById("logoPrincipal");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+    logo.src = "./src/img/logo-color.png";
+  } else {
+    header.classList.remove("scrolled");
+    logo.src = "./src/img/incorporadora-horizontal.png";
+  }
 });
 
 /* ROLAGEM CARROSSEL */
 
-const carousel = document.querySelector('.carousel-inner');
+const carousel = document.querySelector(".carousel-inner");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-carousel.addEventListener('mousedown', (e) => {
-    isDown = true;
-    carousel.classList.add('active');
-    startX = e.pageX - carousel.offsetLeft;
-    scrollLeft = carousel.scrollLeft;
+carousel.addEventListener("mousedown", (e) => {
+  isDown = true;
+  carousel.classList.add("active");
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
 });
 
-carousel.addEventListener('mouseleave', () => {
-    isDown = false;
-    carousel.classList.remove('active');
+carousel.addEventListener("mouseleave", () => {
+  isDown = false;
+  carousel.classList.remove("active");
 });
 
-carousel.addEventListener('mouseup', () => {
-    isDown = false;
-    carousel.classList.remove('active');
+carousel.addEventListener("mouseup", () => {
+  isDown = false;
+  carousel.classList.remove("active");
 });
 
-carousel.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - carousel.offsetLeft;
-    const walk = (x - startX) * 2; //scroll-fast
-    carousel.scrollLeft = scrollLeft - walk;
+carousel.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2; //scroll-fast
+  carousel.scrollLeft = scrollLeft - walk;
 });
 
 /* SCRIPT MODAL */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("filterBtn");
+  var span = document.getElementsByClassName("close")[0];
+  var form = document.getElementById("filterForm");
+  var clearBtn = document.querySelector(".clear-btn");
+  var applyBtn = document.querySelector(".apply-btn");
 
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("filterBtn");
-    var span = document.getElementsByClassName("close")[0];
-    var form = document.getElementById('filterForm');
-    var clearBtn = document.querySelector('.clear-btn');
-    var applyBtn = document.querySelector('.apply-btn');
+  // abre o modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-    // abre o modal
-    btn.onclick = function() {
-        modal.style.display = "block";
+  // fecha o modal no botao de fechar
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // aqui fecha o modal se clica em qualquer lugar fora do modal
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
+  };
 
-    // fecha o modal no botao de fechar
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+  // limpar todos os filtros
+  clearBtn.onclick = function () {
+    var checkboxes = document.querySelectorAll(".modal-checkbox");
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  };
 
-    // aqui fecha o modal se clica em qualquer lugar fora do modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+  // aplicação dos filtros
+  applyBtn.onclick = function () {
+    var cidadeChecked = Array.from(
+      document.querySelectorAll('input[name="cidade"]:checked')
+    ).map((cb) => cb.value);
+    var statusChecked = Array.from(
+      document.querySelectorAll('input[name="status"]:checked')
+    ).map((cb) => cb.value);
+    var dormitorioChecked = Array.from(
+      document.querySelectorAll('input[name="dormitorio"]:checked')
+    ).map((cb) => cb.value);
 
-    // limpar todos os filtros
-    clearBtn.onclick = function() {
-        var checkboxes = document.querySelectorAll('.modal-checkbox');
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = false;
-        });
-    }
+    // aqui eu verifico se o filtro todos está selecionado para resetar todos os filtros
+    if (cidadeChecked.includes("todos")) cidadeChecked = [];
+    if (statusChecked.includes("todos")) statusChecked = [];
+    if (dormitorioChecked.includes("todos")) dormitorioChecked = [];
 
-    // aplicação dos filtros
-    applyBtn.onclick = function() {
-        var cidadeChecked = Array.from(document.querySelectorAll('input[name="cidade"]:checked')).map(cb => cb.value);
-        var statusChecked = Array.from(document.querySelectorAll('input[name="status"]:checked')).map(cb => cb.value);
-        var dormitorioChecked = Array.from(document.querySelectorAll('input[name="dormitorio"]:checked')).map(cb => cb.value);
+    var carouselItems = document.querySelectorAll(".carousel-item");
 
-        // aqui eu verifico se o filtro todos está selecionado para resetar todos os filtros
-        if (cidadeChecked.includes('todos')) cidadeChecked = [];
-        if (statusChecked.includes('todos')) statusChecked = [];
-        if (dormitorioChecked.includes('todos')) dormitorioChecked = [];
+    carouselItems.forEach(function (item) {
+      var cidade = item.getAttribute("data-cidade");
+      var status = item.getAttribute("data-status");
+      var dormitorio = item.getAttribute("data-dormitorio");
 
-        var carouselItems = document.querySelectorAll('.carousel-item');
+      var cidadeMatch =
+        cidadeChecked.length === 0 || cidadeChecked.includes(cidade);
+      var statusMatch =
+        statusChecked.length === 0 || statusChecked.includes(status);
+      var dormitorioMatch =
+        dormitorioChecked.length === 0 ||
+        dormitorioChecked.includes(dormitorio);
 
-        carouselItems.forEach(function(item) {
-            var cidade = item.getAttribute('data-cidade');
-            var status = item.getAttribute('data-status');
-            var dormitorio = item.getAttribute('data-dormitorio');
+      if (cidadeMatch && statusMatch && dormitorioMatch) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
 
-            var cidadeMatch = cidadeChecked.length === 0 || cidadeChecked.includes(cidade);
-            var statusMatch = statusChecked.length === 0 || statusChecked.includes(status);
-            var dormitorioMatch = dormitorioChecked.length === 0 || dormitorioChecked.includes(dormitorio);
-
-            if (cidadeMatch && statusMatch && dormitorioMatch) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        });
-
-        modal.style.display = 'none';
-    }
+    modal.style.display = "none";
+  };
 });
