ccf5bac27f0c527c42089f4c943be7c471b664a2
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-06 17:03:31 -0300

    view: criando página de canal de denuncias

---
+++ canal-de-denuncia-inc.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Template Name: Seja um Fornecedor - Incorporadora
+*/
+include('header-portal-inc.php'); ?>
+
+<section id="banner" class="py-5">
+    <div class="container-fluid">
+        <div class="container-banner">
+            <div class="container-banner-oportunidades__title">
+                <h1>Canal de<br> &ensp;&ensp; Denúncia</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+</main>
+
+<section class="container" id="somos-maximo" style="margin-bottom: 2rem !important;">
+    <div class="container-btns-somos-maximo">
+        <button class="btn-somos-maximo">PORTFÓLIO</button>
+        <button class="btn-somos-maximo">NOSSOS CLIENTES</button>
+        <button class="btn-somos-maximo">CERTIFICAÇÕES</button>
+        <button class="btn-somos-maximo">ORCE SUA OBRA</button>
+    </div>
+    <div class="container-content-oportunidades">
+        <h1 style="margin-bottom: 2rem;">Canal de Denúncia Máximo Aldana</h1>
+        <h2>Mantenha a Integridade em Foco</h2>
+        <h2 style="margin-bottom: 2rem;">Seu Canal Seguro para Denúncias e Transparência</h2>
+        <p>Preencha e envie o formulário ao final da página para fazer uma denúncia.</p>
+    </div>
+    <div class="container-btns-denuncia-inc">
+        <button>FAZER UMA DENÚNCIA <i class="fa-solid fa-arrow-right"></i></button>
+        <button>POLÍTICA DE COMPLIANCE <i class="fa-solid fa-arrow-right"></i></button>
+    </div>
+</section>
+
+<section id="content-denuncias-inc">
+    <div class="container">
+        <div>
+            <p>O Canal de denúncias é voltado para registrar relatos sobre ações e condutas que não estão em conformidade com o Código de Ética e Conduta da Maximo Aldana, assim como com as Leis e regulamentos em vigor. </p>
+            <p>A Maximo Aldana preza pela integridade, transparência e responsabilidade nas relações com todos os seus públicos e pela cultura ética e de valor dentro de nossa organização. </p>
+            <p>Pensando em melhorar nossa comunicação, criamos nosso Canal de Denúncia, onde você poderá sinalizar qualquer conduta que esteja fora do que é ético e que possa prejudicar, você, seu colega ou até mesmo a própria organização</p>
+        </div>
+        <div class="content-tipos-denuncias-inc" style="margin: 2rem 0;">
+            <h5>Este canal destina-se a receber denúncias sobre:</h5><br>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Situações ou suspeitas de atos de corrupção (conforme Lei Anticorrupção nº 12.846/2013). </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Desvios de conduta e assédio. • Descumprimento do Código de Ética e Políticas Corporativas. </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Indícios de ilicitudes que possam afetar a reputação da corporação.</div>
+            <br>
+            <p>A confidencialidade, independência, imparcialidade e não retaliação aos denunciantes de boa-fé são garantidas.</p>
+        </div>
+        <div class="content-tipos-denuncias-inc" style="margin: 2rem 0;">
+            <h5>Requisitos para enquadramento de Denúncia:</h5><br>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Os fatos narrados serão apurados e submetidos à apreciação do Comitê de Conduta e Integridade que é composto pelo Time Jurídico. </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Para que sua denúncia seja atendida com agilidade e eficiência, ela deve vir acompanhada do maior número de informações, como: </div>
+            <br>
+            <p>Descrição clara do ocorrido;</p>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>A data e local do fato; </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Quem são as pessoas envolvidas (pertencentes ou não à organização);</div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Evidências: documentos ou informações relevantes sobre o ocorrido.</div>
+        </div>
+        <div class="content-tipos-denuncias-inc" style="margin: 2rem 0;">
+            <h5>Como é tratada a Denúncia:</h5>
+            <br>
+            <p>Primeiro recebemos a denúncia pelo Canal de Denuncia;</p>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Em seguida a denúncia é avaliada pelo comitê; </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Após a avaliação é formulado um plano de ação para prosseguir com a investigação; </div>
+            <div class="content-tipos-paragrafo-inc"><i class="fa-solid fa-check" style="color: #18354f;"></i>Após conclusão da investigação as ações serão tomadas para cessar a violação. </div>
+            <br><br>
+            <p>Este canal é totalmente confidencial e anônimo, então, não se sinta reprimido na hora de fazer sua denúncia. </p>
+            <p>A ferramenta é segura e confidencial e as informações relatadas são recebidas de forma sigilosa. Em seguida, encaminhadas para avaliação dos responsáveis. Deseja relatar um desvio de conduta? Basta preencher o formulário abaixo:</p>
+        </div>
+        <div class="content-container-form-portal-inc">
+            <form action="">
+                <div class="form-group">
+                    <label for="login">Deseja se identificar? Se sim, escreva seu nome abaixo:</label>
+                    <input type="text" class="form-control" placeholder="Nome Completo" id="nome" aria-describedby="nome" required>
+                </div>
+                <div class="form-group">
+                    <label for="telefone">Natureza da Ocorrência:*</label>
+                    <input type="text" class="form-control" placeholder="Nome da empresa" id="nome-empresa" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">CNPJ:*</label>
+                    <input type="email" class="form-control" placeholder="CNPJ" id="CNPJ" required>
+                </div>
+                <div class="form-group">
+                    <label for="login">Telefone?*</label>
+                    <input type="text" class="form-control" placeholder="+55 (00) 00000-0000" id="nome" aria-describedby="nome" required>
+                </div>
+                <div class="form-group">
+                    <label for="telefone">E-mail:*</label>
+                    <input type="text" class="form-control" placeholder="E-mail" id="Email" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Segmento da Empresa</label>
+                    <input type="email" class="form-control" placeholder="Segmento da Empresa" id="segmento-empresa" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Qual a melhor forma de contato?</label><br>
+                    <div class="label-radio-fale-conosco-inc">
+                        <div class="input-radio-fale-conosco-inc">
+                            <input type="radio" id="contactChoice2" name="contact" value="Whatsapp">
+                            <label for="">Whatsapp</label>
+                        </div>
+                        <div class="input-radio-fale-conosco-inc">
+                            <input type="radio" id="contactChoice2" name="contact" value="E-mail">
+                            <label for="">E-mail</label>
+                        </div>
+                        <div class="input-radio-fale-conosco-inc">
+                            <input type="radio" id="contactChoice2" name="contact" value="Telefone">
+                            <label for="">Telefone</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="telefone">Escreva sua mensagem:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Escreva aqui" rows="4"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="">Faça upload:</label><br>
+                </div>
+                <p style="padding: 0 2rem; color: white;">Capacidade máxima de até 25MB. Aceitamos arquivos do tipo jpeg. pgn. pdf. ppt. mp4.</p><br>
+                <div class="inputs-file-seu-terreno-inc">
+                    <input class="input-file-inc-terreno" type="file" id="file-upload" name="file-upload" multiple accept=".jpeg,.jpg,.png,.pgn,.pdf,.ppt,.mp4" required>
+                </div>
+                <button type="submit">Enviar</button>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="lembrarSenha">
+                    <label class="form-check-label" for="lembrarSenha">Ao preencher e enviar este formulário, você concorda com o armazenamento e manuseio de seus dados pelo Máximo Aldana</label>
+                </div>
+            </form>
+        </div>
+    </div> 
+</section>
+
+
+<?php include('footer-inc.php'); ?>
