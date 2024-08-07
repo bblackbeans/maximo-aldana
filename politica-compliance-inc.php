5c730d33870f9e4a2581a4335a54526e988433d6
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-07 17:04:57 -0300

    view: criando páginas de termos de uso e politica compliance

---
+++ politica-compliance-inc.php
@@ -0,0 +1,188 @@
+<?php
+/*
+Template Name: Política de Compliance - Incorporadora
+*/
+include('header-portal-inc.php'); ?>
+
+<section id="banner" class="py-5">
+    <div class="container-fluid">
+        <div class="container-banner">
+            <div class="container-banner-oportunidades__title">
+                <h1>Política de<br> &ensp;&ensp; Compliance</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+</main>
+
+<section class="container" id="somos-maximo" style="margin-bottom: 2rem !important;">
+    <div class="container-btns-somos-maximo">
+        <button class="btn-somos-maximo">PORTFÓLIO</button>
+        <button class="btn-somos-maximo">NOSSOS CLIENTES</button>
+        <button class="btn-somos-maximo">CERTIFICAÇÕES</button>
+        <button class="btn-somos-maximo">ORCE SUA OBRA</button>
+    </div>
+    <div class="container-content-oportunidades">
+        <h1 style="margin-bottom: 2rem;">Política de Compliance da Máximo Aldana</h1>
+        <h2>Compromisso com a Excelência e Ética</h2>
+        <h2 style="margin-bottom: 2rem;">Nossas Práticas de Compliance Garantem Negócios Justus e Transparentes</h2>
+        <p>Clique no botão abaixo para baixar a política de compliance da Máximo Aldana</p>
+        <button class="btn-baixar-compliance-inc">BAIXAR POLÍTICA DE COMPLIANCE</button>
+    </div>
+
+    <div class="container-btns-denuncia-inc">
+        <button>SOBRE A MAXIMO ALDANA <i class="fa-solid fa-arrow-right"></i></button>
+        <button>CANAL DE DENÚNCIAS <i class="fa-solid fa-arrow-right"></i></button>
+    </div>
+</section>
+
+<section id="blog-home" class="pb-5">
+    <div class="container-fluid px-2">
+        <div class="container-title-blog">
+            <h4>Fique por dento do</h4>
+            <h2>Nosso Blog</h2>
+        </div>
+        <div class="container container-card-blog">
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/novoContrato.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana pela terceira vez consecutiva, entre as 100 maiores construtoras do Brasil segundo o Ranking Intec</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>3 de Março de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/100maiores.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana construirá o novo Hospital de Unimed Sul Mineira em Pouso Alegre - MG</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/premiohealtharq.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana é destaque no HealthARQ 2022</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container container-card-blog second-card-blog" style="display: none;">
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/novoContrato.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana pela terceira vez consecutiva, entre as 100 maiores construtoras do Brasil segundo o Ranking Intec</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>3 de Março de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Ranking foi divulgado hoje, no dia 1 de março de 2023. A construtora e incorporadora ficou em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/100maiores.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana construirá o novo Hospital de Unimed Sul Mineira em Pouso Alegre - MG</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>16 de Janeiro de 2023 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Em breve a população de Pouso Alegre (MG) e região passará a contar com o hospital da Unimed...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="" style="max-width: 400px; height:605px;">
+                <div class="card-blog">
+                    <div class="card-blog__img">
+                        <img class="img-fluid" src="./src/img/premiohealtharq.png" alt="">
+                    </div>
+                    <div class="card-blog__title">
+                        <h2>Maximo Aldana é destaque no HealthARQ 2022</h2>
+                    </div>
+                    <div class="card-blog__data">
+                        <span class="material-symbols-outlined">
+                            calendar_today
+                        </span>
+                        <p>11 de Novembro de 2022 • Por Máximo Aldana</p>
+                    </div>
+                    <div class="card-blog__desc">
+                        Essa semana a Maximo Aldana recebeu o Prêmio HealthARQ 2022 na categoria Inovação em...
+                    </div>
+                    <div class="card-blog__button">
+                        <a href="" class="bg-acquagreen">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-button-todos">
+            <button class="ver-todos-button bg-acquagreen" onclick="toggleCardBlog()">VER TODOS</button>
+        </div>
+    </div>
+</section>
+
+<?php include('footer-inc.php'); ?>
