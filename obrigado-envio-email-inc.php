9b26e907a5882ca8dfe0a6db68160711453d06f7
KaueRonald <Kaueronald21.kr@gmail.com>
2024-08-08 16:05:04 -0300

    view: criando páginas de obrigado

---
+++ obrigado-envio-email-inc.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Página Obrigado Envio Email Incorporadora
+*/
+include('header-agradecimento-inc.php'); ?>
+
+<section id="banner" class="py-5">
+    <div class="container">
+        <div class="text-agradecimento-inc">
+            <h2>A Maximo Aldana agradece sua inscrição!</h2>
+            <h3>Recebemos suas informações.</h3>
+            <p>A partir de agora você passa a receber todas as newsletter da Máximo Aldana</p>
+        </div>
+    </div>
+</section>
+
+</main>
+
+<section class="container" id="somos-maximo" style="margin-bottom: 2rem !important;">
+    <div class="container-btns-denuncia-inc">
+        <button style="padding-left: 4rem;">VOLTAR PARA HOME <i class="fa-solid fa-arrow-right"></i></button>
+    </div>
+</section>
+
+<?php include('footer-inc.php'); ?>
